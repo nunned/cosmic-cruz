e90241e273a5c33a17f8437a9a8571a42c068c7b
nunned <chanonac@outlook.com>
2024-01-04 13:27:05 -0600

    updated homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,27 +1,43 @@
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import './styles/homepage.css'; // Assuming you'll store styles in HomePage.css
+import './styles/homepage.css';
 
 function HomePage() {
+  const navItems = [
+    { route: "/barberservice", label: "Barbering" },
+    { route: "/apparel", label: "Apparel" },
+    { route: "/travelguide", label: "Travel Guides" },
+    { route: "/visuals", label: "Visuals" },
+    { route: "/about", label: "About Me" }
+  ];
+
+  // Adjust the position and hover effect for each item
+  const itemStyles = [
+    { top: "20%", left: "50%", hoverY: [0, -10] },
+    { top: "55%", left: "20%", hoverY: [0, -10] },
+    { top: "40%", left: "80%", hoverY: [0, 10] },
+    { top: "60%", left: "50%", hoverY: [0, 10] },
+    { top: "80%", left: "30%", hoverY: [0, -10] }
+    // Adjust these values to suit your layout
+  ];
+
   return (
     <div className='bg-wrap'>
-    <div className="layer1"></div> {/* First additional layer */}
-      <div className="layer2"></div> {/* Second additional layer */}
+      <div className="layer1"></div>
+      <div className="layer2"></div>
       <nav className="nav-list">
-        <div className="item-container">
-          <Link to="/barberservice" className="nav-sphere">Barbering</Link>
-        </div>
-        <div className="item-container">
-          <Link to="/apparel" className="nav-sphere">Apparel</Link>
-        </div>
-        <div className="item-container">
-          <Link to="/travelguide" className="nav-sphere">Travel Guides</Link>
-        </div>
-        <div className="item-container">
-          <Link to="/visuals" className="nav-sphere">Visuals</Link>
-        </div>
-        <div className="item-container">
-          <Link to="/about" className="nav-sphere">About Me</Link>
-        </div>
+        {navItems.map((item, index) => (
+          <motion.div
+            key={index}
+            className="item-container"
+            style={{ top: itemStyles[index].top, left: itemStyles[index].left }}
+            initial={{ y: 0 }}
+            animate={{ y: itemStyles[index].hoverY }}
+            transition={{ repeat: Infinity, duration: 2, ease: "easeInOut", repeatType: "reverse" }}
+          >
+            <Link to={item.route} className="nav-sphere">{item.label}</Link>
+          </motion.div>
+        ))}
       </nav>
     </div>
   );
